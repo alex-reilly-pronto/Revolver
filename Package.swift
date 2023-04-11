3b61b2bc635d56e1349b342a2ff365901573ccef
Alex Reilly <alex.reilly@pronto.ai>
2023-04-11 15:39:06 -0700

    add package product

--- Package.swift
+++ Package.swift
@@ -4,6 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "Revolver",
+    products: [
+      .library(
+        name: "Revolver",
+        targets: ["Revolver"])
+    ],
     dependencies: [
       .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.0")
     ],
